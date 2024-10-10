bd4d72837c858e63184332c0648476a84adaae54
HanzelMae <153923267+HanzelMae@users.noreply.github.com>
2024-10-10 23:55:33 +0800

    Delete New folder/script.js

--- New folder/script.js
+++
@@ -1,8 +0,0 @@
-const totalUsers = document.getElementById('totalUsers').textContent;
-const totalSales = document.getElementById('totalSales').textContent;
-
-const alertButton = document.getElementById('alertButton');
-
-alertButton.addEventListener('click', function() {
-    alert(`Total Users: ${totalUsers}, Total Sales: ${totalSales}`);
-});
