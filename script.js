635a4141d5a888afb80f6e8612c0036e2577fbc7
HanzelMae <153923267+HanzelMae@users.noreply.github.com>
2024-10-10 23:53:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const totalUsers = document.getElementById('totalUsers').textContent;
+const totalSales = document.getElementById('totalSales').textContent;
+
+const alertButton = document.getElementById('alertButton');
+
+alertButton.addEventListener('click', function() {
+    alert(`Total Users: ${totalUsers}, Total Sales: ${totalSales}`);
+});
